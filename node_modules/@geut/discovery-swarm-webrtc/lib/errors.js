666c8a67fec5e9daf3bf6cf819f6603ae8caf4f8
lanmower <almagestfraternite@gmail.com>
2021-09-06 13:35:28 +0000

    first commit

--- node_modules/@geut/discovery-swarm-webrtc/lib/errors.js
+++
@@ -1,9 +0,0 @@
-const nanoerror = require('nanoerror')
-
-function createError (code, message) {
-  exports[code] = nanoerror(code, message)
-}
-
-createError('ERR_MAX_PEERS_REACHED', 'max peers reached: %s')
-createError('ERR_INVALID_CHANNEL', 'invalid channel: %s')
-createError('ERR_CONNECTION_DUPLICATED', 'connection duplicated: %s -> %s')
--- node_modules/@geut/discovery-swarm-webrtc/server.js
+++
@@ -1,5 +0,0 @@
-const { SocketSignalWebsocketServer } = require('socket-signal-websocket')
-
-// Backward compatibility with v2
-module.exports = (...args) => new SocketSignalWebsocketServer(...args)
-module.exports.SignalServer = SocketSignalWebsocketServer
